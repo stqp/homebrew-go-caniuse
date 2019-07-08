5bed6e99014b95c4817bf9625caf94c74d59028c
stqp <1405922+stqp@users.noreply.github.com>
2019-07-08 21:43:21 +0900

    Update go-caniuse.rb

--- go-caniuse.rb
+++ go-caniuse.rb
@@ -1,15 +1,15 @@
 class GoCaniuse < Formula
   desc "A simple command line tool for caniuse ."
-  homepage ""
-	version "1.0.0"
+  homepage "https://github.com/stqp/go-caniuse"
+  version "1.0.0"
   url "https://github.com/stqp/go-caniuse/releases/download/v1.0.0/go-caniuse_1.0.0_darwin_amd64.tar.gz"
   sha256 "f417278eabcc40bd9cd0275a10884df064fd32d2e63f67f46d2686740380f761"
 
   def install
-		bin.install "go-caniuse"
+    bin.install 'go-caniuse'
   end
 
   test do
-		system "#{bin}/program --version"
+    system "#{bin}/program --version"
   end
 end
